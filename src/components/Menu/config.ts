53f0c9204179eb706a2e4a0e54be6a418f5bab5a
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-04-23 10:01:50 -0700

    updated menu config

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -87,4 +87,85 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
   },
 ]
 
+export const bttConfig: (t: ContextApi['t']) => MenuEntry[] = (t) => [
+  {
+    label: t('Home'),
+    icon: 'HomeIcon',
+    href: '/home',
+  },
+  {
+    label: t('Trade'),
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: t('Exchange'),
+        href: '/swap',
+      },
+      {
+        label: t('Liquidity'),
+        href: '/pool',
+      },
+    ],
+  },
+  // {
+  //   label: t('Farms'),
+  //   icon: 'FarmIcon',
+  //   href: '/farms',
+  // },
+  // {
+  //   label: t('Staking Pools'),
+  //   icon: 'PoolIcon',
+  //   href: '/staking',
+  // },
+  // {
+  //   label: t('Launchpads'),
+  //   icon: 'LaunchpadIcon',
+  //   href: '/launchpad',
+  // },
+  {
+    label: t('Bridge'),
+    icon: 'BridgeIcon',
+    href: 'https://callistobridge.netlify.app/',
+    target: "_blank"
+  },
+  // {
+  //   label: t('CHOAM'),
+  //   href: 'https://choamtoken.com/',
+  //   icon: "ChoamIcon",
+  //   target: "_blank",
+  //   status: menuStatus.SOLDOUT
+  // },
+  // {
+  //   label: t('NFT'),
+  //   icon: 'NftIcon',
+  //   href: '/nft',
+  // },
+  // {
+  //   label: t('Info'),
+  //   icon: 'InfoIcon',
+  //   href: '/info'
+  // },
+  {
+    label: t('More'),
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: t('Github'),
+        href: 'https://github.com/SoyFinance',
+        target: "_blank"
+      },
+      {
+        label: t('Docs'),
+        href: 'https://callisto.network/',
+        target: "_blank"
+      },
+      {
+        label: t('Blog'),
+        href: 'https://callisto.network/blog/',
+        target: "_blank"
+      },
+    ],
+  },
+]
+
 export default config
--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -3,14 +3,20 @@ import { Menu as UikitMenu } from '@soy-libs/uikit2'
 import { languageList } from 'config/localization/languages'
 import { useTranslation } from 'contexts/Localization'
 import useTheme from 'hooks/useTheme'
-// import useGetPriceData from 'hooks/useGetPriceData'
+import useActiveWeb3React from 'hooks/useActiveWeb3React'
 import { usePriceCakeBusd } from 'state/farms/hooks'
 import { useProfile } from 'state/profile/hooks'
 import { addSoyToMetamask } from 'utils/wallet'
-import config from './config'
+import config, { bttConfig } from './config'
 import UserMenu from './UserMenu'
 
+// const configs = {
+//   820: config,
+//   199: bttConfig,
+// }
+
 const Menu = (props) => {
+  const { chainId } = useActiveWeb3React()
   const { isDark, toggleTheme } = useTheme()
   const soyPriceUsd = usePriceCakeBusd()
   const { profile } = useProfile()
@@ -27,7 +33,7 @@ const Menu = (props) => {
       langs={languageList}
       setLang={setLanguage}
       cakePriceUsd={soyPriceUsd.toNumber()}
-      links={config(t)}
+      links={chainId === 199 ? bttConfig(t) : config(t)}
       profile={{
         username: profile?.username,
         image: profile?.nft ? `/images/nfts/${profile.nft?.images.sm}` : undefined,
