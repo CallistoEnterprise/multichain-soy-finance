7586b926b7fa1eac1250726180276be76c53cec2
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-06-27 23:12:59 -0700

    updated public logos

--- src/state/profile/getProfile.ts
+++ src/state/profile/getProfile.ts
@@ -54,9 +54,9 @@ const getProfile = async (address: string): Promise<GetProfileResponse> => {
         `profile_${address}`,
         {
           username,
-          avatar: `https://pancakeswap.finance/images/nfts/${nft?.images.sm}`,
+          avatar: `https://app.soy.finance/images/nfts/${nft?.images.sm}`,
         },
-        { domain: 'pancakeswap.finance', secure: true, expires: 30 },
+        { domain: 'soyswap.finance', secure: true, expires: 30 },
       )
     }
 
