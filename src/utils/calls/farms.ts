7516e7192f996c203955f93ec5095d5ab794cdae
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-06-30 10:26:54 -0700

    updated farms actions

--- src/utils/calls/farms.ts
+++ src/utils/calls/farms.ts
@@ -1,27 +1,30 @@
 import BigNumber from 'bignumber.js'
-import { DEFAULT_TOKEN_DECIMAL } from 'config'
-import { callWithEstimateGas } from './estimateGas'
+import { DEFAULT_TOKEN_DECIMAL, DEFAULT_GAS_LIMIT } from 'config'
+// import { callWithEstimateGas } from './estimateGas'
 
-// const options = {
-//   gasLimit: DEFAULT_GAS_LIMIT,
-// }
+const options = {
+  gasLimit: DEFAULT_GAS_LIMIT + 100000,
+}
 
 export const stakeFarm = async (lpContract, localFarmAddress, amount) => {
   const value = new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()
-  const tx = await callWithEstimateGas(lpContract, 'transfer', [localFarmAddress, value])
+  // const tx = await callWithEstimateGas(lpContract, 'transfer', [localFarmAddress, value])
+  const tx = await lpContract.transfer(localFarmAddress, value, options)
   const receipt = await tx.wait()
   return receipt.status
 }
 
 export const unstakeFarm = async (localFarmContract, amount) => {
   const value = new BigNumber(amount).times(DEFAULT_TOKEN_DECIMAL).toString()
-  const tx = await callWithEstimateGas(localFarmContract, 'withdraw', [value])
+  // const tx = await callWithEstimateGas(localFarmContract, 'withdraw', [value])
+  const tx = await localFarmContract.withdraw(value, options)
   const receipt = await tx.wait()
   return receipt.status
 }
 
 export const harvestFarm = async (lpContract, localFarmAddress) => {
-  const tx = await callWithEstimateGas(lpContract, 'transfer', [localFarmAddress, '0'])
+  // const tx = await callWithEstimateGas(lpContract, 'transfer', [localFarmAddress, '0'])
+  const tx = await lpContract.transfer(localFarmAddress, '0', options)
   const receipt = await tx.wait()
   return receipt.status
 }
