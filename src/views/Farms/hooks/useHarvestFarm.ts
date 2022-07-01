6ce54dc589b9cb801648473d82bab2854e2d1e94
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-07-01 02:46:26 -0700

    update harvest farm action

--- src/views/Farms/hooks/useHarvestFarm.ts
+++ src/views/Farms/hooks/useHarvestFarm.ts
@@ -1,4 +1,5 @@
-import { useWeb3React } from '@web3-react/core'
+// import { useWeb3React } from '@web3-react/core'
+import useActiveWeb3React, { useWeb3ProviderByRpc } from 'hooks/useActiveWeb3React'
 import { localStorageChainIdKey } from 'config'
 import farms from 'config/constants/farms'
 import { useCallback } from 'react'
@@ -7,16 +8,17 @@ import { harvestFarm } from 'utils/calls'
 import { getLpContractWithAccount } from 'utils/contractHelpers'
 
 const useHarvestFarm = (farmPid: number) => {
-  const { account, library } = useWeb3React()
+  const { account, library } = useActiveWeb3React()
   const chainId = Number(window.localStorage.getItem(localStorageChainIdKey) ?? '820')
   const currentFarm = farms[chainId].find((farm) => farm.pid === farmPid)
   const { lpAddresses, localFarmAddresses }= currentFarm
   const lpContract = getLpContractWithAccount(getAddress(lpAddresses), library, account)
   const farmAddress = getAddress(localFarmAddresses)
+  const web3 = useWeb3ProviderByRpc(chainId)
 
   const handleHarvest = useCallback(async () => {
-    await harvestFarm(lpContract, farmAddress)
-  }, [farmAddress, lpContract])
+    await harvestFarm(lpContract, farmAddress, web3)
+  }, [farmAddress, lpContract, web3])
 
   return { onReward: handleHarvest }
 }
