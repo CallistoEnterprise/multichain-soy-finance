70a8abd56d31d819526135f03f2498b1c6d60776
mobiledev-111-118 <mobiledev111118@gmail.com>
2022-09-23 09:03:28 -0700

    fixed small issue related with DEFAULT_CHAINID

--- src/App.tsx
+++ src/App.tsx
@@ -86,6 +86,7 @@ const App: React.FC = () => {
   usePollCoreFarmData()
 
   useEffect(() => {
+    window.localStorage.removeItem('soyfinanceChainId')
     const init = async () => {
       setupNetwork2()
     }
