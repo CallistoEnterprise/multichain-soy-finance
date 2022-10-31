45900f86747e25f8eeab2bab27428aea26fbc38e
MJ <80132692+MJ-UKR@users.noreply.github.com>
2022-10-31 16:28:29 +0200

    update CZ node

--- src/config/constants/chains/etc.ts
+++ src/config/constants/chains/etc.ts
@@ -17,7 +17,7 @@ const ETC_CONSTANTS: ChainConstants = {
     nativeAddress: '0x0000000000000000000000000000000000000005',
     wrappedNativeAddress: '0x35e9A89e43e45904684325970B2E2d258463e072',
   },
-  rpcs: ['https://etc.etcdesktop.com/'],
+  rpcs: ['https://01.callisto.network/etcnode2'],
   explorer: {
     name: 'ETCScan',
     url: 'https://blockscout.com/etc/mainnet/',
