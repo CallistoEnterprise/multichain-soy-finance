55178ee5e66743698e9ae3451e4d4eb105742369
Yuriy <yuriy77k@gmail.com>
2023-07-17 20:38:40 +0300

    ETC RPC

--- src/config/constants/chains/etc.ts
+++ src/config/constants/chains/etc.ts
@@ -17,7 +17,7 @@ const ETC_CONSTANTS: ChainConstants = {
     nativeAddress: '0x0000000000000000000000000000000000000005',
     wrappedNativeAddress: '0x35e9A89e43e45904684325970B2E2d258463e072',
   },
-  rpcs: ['https://etc.etcdesktop.com', 'https://geth-at.etc-network.info', 'https://01.callisto.network/etcnode2'],
+  rpcs: ['https://etc.etcdesktop.com'], // ,'https://geth-at.etc-network.info', 'https://01.callisto.network/etcnode2'
   explorer: {
     name: 'ETCScan',
     url: 'https://blockscout.com/etc/mainnet/',
